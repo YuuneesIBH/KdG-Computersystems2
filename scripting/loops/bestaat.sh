af7af8cb5875077ac0d9eb99f2a2d1cb1143989b
yuunees.s <younes.elazzouzi@student.kdg.be>
2025-07-15 19:57:04 +0200

    FINISHED: loops first pdf

--- scripting/loops/bestaat.sh
+++ scripting/loops/bestaat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Functie: Controleert of /bin/df uitvoerbaar is, en voert het uit met -h
-# Auteur: younes.elazzouzi0student.kdg.be
+# Auteur: younes.elazzouzi@student.kdg.be
 # Versie: 0.1
 # Datum: 2025-07-15
 
