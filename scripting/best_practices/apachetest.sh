856ab25ba002b60296ecee154a24cd2992d1c828
yuunees.s <younes.elazzouzi@student.kdg.be>
2025-07-15 20:55:24 +0200

    BEGIN: best_practices

---
+++ scripting/best_practices/apachetest.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Auteur: younes.elazzouzi@student.kdg.be
+# Versie: 0.1
+# Datum: 2025-07-15
+
+error_geen_ab="Fout in $0: het programma 'ab' is niet geïnstalleerd."
+error_url="de url is niet bereikbaar"
+
+command -v ab >/dev/null || { echo "$error_geen_ab" >&2; exit 1; }
+
+#gebruik een standaard URL als er geen wordt meegegeven
+url=${1:-"http://127.0.0.1/"}
+
+curl -o /dev/null --silent --head --connect-timeout 2 ${url}
+if [ $? -ne 0 ]; then
+    echo "$error_url" >&2
+    exit 2
+fi
+
+#benchmark uitvoeren
+ab -n 100 -kc 10 "$url"
---
+++ scripting/loops/TODOMP4.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Auteur: younes.elazzouzi@student.kdg.be
+# Versie: 0.1
+# Datum: 2025-07-15
